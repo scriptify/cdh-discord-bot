81ffcb54284a695afc153c65abd9de9e9872e904
Maximilian Torggler <inquiries@maximilian-torggler.dev>
2021-11-02 18:17:36 +0100

    it is live

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 
+const NUM_HIPSTERS = 10000;
+
 dotenv.config();
 const client = new Discord.Client({
   intents: [
@@ -16,10 +18,28 @@ function main() {
   });
 
   client.on("messageCreate", (msg) => {
-    if (msg.content === "test") {
-      msg.reply(
-        "https://ogster.vercel.app/og-test/QmSYstvjJG1V15fneLQpzbqyTQbwpTvY2whwvvZ2ZrMasA"
+    if (msg.content.includes("#")) {
+      const allOccurrences = [
+        ...msg.content.matchAll(new RegExp(/#[0-9]\d*\b/, "gi")),
+      ].map((oc) => oc.toString());
+
+      const hipsterIndices = allOccurrences
+        .map((indexStr) => {
+          const parsed = parseInt(indexStr.replace("#", ""));
+          if (isNaN(parsed)) {
+            return null;
+          }
+          return parsed;
+        })
+        .filter((num) => num !== null && num < NUM_HIPSTERS);
+
+      const linksToPost = hipsterIndices.map(
+        (index) => `https://www.cardanohipsters.io/collection/${index}`
       );
+
+      for (const link of linksToPost) {
+        msg.reply(link);
+      }
     }
   });
 
